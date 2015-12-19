cd0f1327c7a4acbbe42a18ced2d8d10dd2283a29
aantoine <antoineagustin@gmail.com>
2015-12-19 18:09:21 -0300

    subqueries are now loaded correctly

--- app/LabControl/static/LabControl/blockly/subqueries.js
+++ app/LabControl/static/LabControl/blockly/subqueries.js
@@ -1,18 +1,21 @@
-var subQueries = {};
-var subQueriesList = [];
-var subQueriesTypes = {};
-
 function dynamicOptions(){
-    if(subQueriesList == false){
-        return [["", null]];
+	var subQueriesList = [];
+	if(Object.keys(subqueries).length==0){
+		return [["",null]];
+	}
+	for(var name in subqueries){
+		subQueriesList.push([name,name]);
     }
+    console.log("List:"+subQueriesList);
 	return subQueriesList;
 }
 
 function getStatementType(name){
-    return subQueriesTypes[name];
+    return subqueries[name]["type"];
 }
 
 function getSubqueryCode(name){
-	return subQueries[name];
+	var query = subqueries[name]["query"];
+	query = query.split("'").join("\"")
+	return query;
 }
